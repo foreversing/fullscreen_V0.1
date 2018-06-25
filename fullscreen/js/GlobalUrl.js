79a8526f410f27590e838b78fdce49fe6534009a
foreversing <31986290+foreversing@users.noreply.github.com>
2018-06-25 14:14:05 +0800

    Initial commit

---
+++ fullscreen/js/GlobalUrl.js
@@ -0,0 +1,7 @@
+/*
+var globalUrlBigDatajy = "http://mercury.cm-inv.com/cm_bdp/projectProgress/container4";
+var globalUrlJiayetest = "http://mercury.cm-inv.com/cm_bdp/projectProgress/container2";
+var globalUrl = "http://jupiter.cm-inv.com";*/
+var globalUrlBigDatajy = "http://172.26.128.74:8080/cm_bdp/projectProgress/container4";
+var globalUrlJiayetest = "http://172.26.128.74:8080/cm_bdp/projectProgress/container3";
+var globalUrl = "http://172.26.128.56:8080";
